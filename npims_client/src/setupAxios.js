41d608dfc294ec55252082c98a0d1da4090febfe
Melisse Deocareza <mgdeocareza@up.edu.ph>
2025-05-29 13:48:02 +0800

    add debug message for axios

--- npims_client/src/setupAxios.js
+++ npims_client/src/setupAxios.js
@@ -1,4 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000";
-axios.defaults.withCredentials = true; 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+console.log("Axios baseURL is set to:", axios.defaults.baseURL);
+
+export default axios;
